6e2b06effbb980d61b844b23494ec5aa4a5af99f
AlanCarlosjp <alandepaulajr@gmail.com>
2023-07-13 18:31:13 -0300

    base chart

--- src/pages/GraficsScreen/index.tsx
+++ src/pages/GraficsScreen/index.tsx
@@ -1,13 +1,41 @@
 import { FC } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Dimensions } from "react-native";
 import styles from "./styles";
-import CredCard from "../../component/CredCard";
-
+import {
+  BarChart,
+} from 'react-native-chart-kit';
 
 const GraficsScreen: FC = () => (
-    <View style={styles.container}>
-    </View>
-  );
+  <View style={styles.container}>
+    <Text>Bar Chart</Text>
+    <BarChart
+      data={{
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            data: [20, 45, 28, 80, 99, 43],
+          },
+        ],
+      }}
+      width={Dimensions.get('window').width - 16}
+      height={220}
+      yAxisLabel={'$'}
+      chartConfig={{
+        backgroundColor: '#e26a00',
+        backgroundGradientFrom: '#fb8c00',
+        backgroundGradientTo: '#ffa726',
+        decimalPlaces: 2,
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16,
+        },
+      }}
+      style={{
+        marginVertical: 8,
+        borderRadius: 16,
+      }}
+    />
+  </View>
+);
 
-  
 export default GraficsScreen;
