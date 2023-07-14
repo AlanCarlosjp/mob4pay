630558aac4fed5d3ff46726695bfea41be07527c
AlanCarlosjp <alandepaulajr@gmail.com>
2023-07-14 14:52:52 -0300

    card with sum

--- src/component/CredCard/index.tsx
+++ src/component/CredCard/index.tsx
@@ -1,14 +1,15 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import styles from './styles';
 import ClientInfoService from '../../service/ClientInfoService';
-import { ClientInfo } from '../../../types';
+import PurchaseService from '../../service/Purchases';
+import { ClientInfo, Purchase } from '../../../types';
 import formatDate from '../../utils/FormatDate';
-
-
 
 const CredCard = () => {
     const [clientInfo, setClientInfo] = useState<ClientInfo | null>(null);
+    const [purchases, setPurchases] = useState<Purchase[]>([]);
+    const [isCardFlipped, setCardFlipped] = useState<boolean>(false);
 
     useEffect(() => {
         ClientInfoService.getClientInfo().then((data) => {
@@ -16,7 +17,11 @@ const CredCard = () => {
                 setClientInfo(data[0]);
             }
         });
+
+        PurchaseService.getPurchases().then(setPurchases);
     }, []);
+
+    const totalPurchaseValue = purchases.reduce((sum, purchase) => sum + purchase.value, 0);
 
     if (!clientInfo) {
         return (
@@ -27,19 +32,27 @@ const CredCard = () => {
     }
 
     return (
-        <ImageBackground
-            source={require('../../../assets/card-front.png')}
-            style={styles.card}
-            resizeMode="cover"
-        >
-            <View style={styles.cardContent}>
-                <Text style={styles.cardNumber}>{clientInfo.cardNumber}</Text>
-                <View style={styles.cardDetails}>
-                    <CardDetail title="Valid Thru" content={formatDate(clientInfo.expirationDate)} />
-                    <CardDetail title="Card Holder" content={clientInfo.name} />
+        <TouchableOpacity onPress={() => setCardFlipped(!isCardFlipped)}>
+            <ImageBackground
+                source={require('../../../assets/card-front.png')}
+                style={styles.card}
+                resizeMode="cover"
+            >
+                <View style={styles.cardContent}>
+                    {isCardFlipped ? (
+                        <Text style={styles.cardNumber}>Total de gastos: R${totalPurchaseValue.toFixed(2)}</Text>
+                    ) : (
+                        <>
+                            <Text style={styles.cardNumber}>{clientInfo.cardNumber}</Text>
+                            <View style={styles.cardDetails}>
+                                <CardDetail title="Valid Thru" content={formatDate(clientInfo.expirationDate)} />
+                                <CardDetail title="Card Holder" content={clientInfo.name} />
+                            </View>
+                        </>
+                    )}
                 </View>
-            </View>
-        </ImageBackground>
+            </ImageBackground>
+        </TouchableOpacity>
     );
 }
 
